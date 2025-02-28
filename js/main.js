d455bd02631885b2c4667889cf9b6589473eb33d
Telphii <zambezikrv@yandex.com>
2025-02-28 10:25:52 +0700

    Review

--- js/main.js
+++ js/main.js
@@ -12,6 +12,79 @@ Vue.component('product-details', {
     `
 });
 
+Vue.component('product-review', {
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+            <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" v-model="name" placeholder="name" required>
+            </p>
+            <p>
+                <label for="review">Review:</label>
+                <textarea id="review" v-model="review" required></textarea>
+            </p>
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="rating" required>
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+            <p>
+                <label>Would you recommend this product?</label>
+                <input type="radio" id="recommend-yes" value="yes" v-model="recommend">
+                <label for="recommend-yes">Yes</label>
+                <input type="radio" id="recommend-no" value="no" v-model="recommend">
+                <label for="recommend-no">No</label>
+            </p>
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            this.errors = [];
+            if (!this.name) this.errors.push("Name required.");
+            if (!this.review) this.errors.push("Review required.");
+            if (!this.rating) this.errors.push("Rating required.");
+            if (!this.recommend) this.errors.push("Recommendation required.");
+
+            if (this.errors.length === 0) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                };
+                this.$emit('review-submitted', productReview);
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+                this.recommend = null;
+            }
+        }
+    }
+});
+
 Vue.component('product', {
     props: {
         premium: {
@@ -54,6 +127,21 @@ Vue.component('product', {
                     @mouseover="updateProduct(index)"
                 >
                 </div>
+
+                <div>
+                    <h2>Reviews</h2>
+                    <p v-if="!reviews.length">There are no reviews yet.</p>
+                    <ul>
+                        <li v-for="review in reviews">
+                            <p>{{ review.name }}</p>
+                            <p>Rating: {{ review.rating }}</p>
+                            <p>{{ review.review }}</p>
+                            <p>Recommend: {{ review.recommend }}</p>
+                        </li>
+                    </ul>
+                </div>
+
+                <product-review @review-submitted="addReview"></product-review>
             </div>
         </div>
     `,
@@ -81,7 +169,8 @@ Vue.component('product', {
                     variantImage: "./assets/vmSocks-blue-onWhite.jpg",
                     variantQuantity: 0
                 }
-            ]
+            ],
+            reviews: []
         };
     },
     computed: {
@@ -107,6 +196,9 @@ Vue.component('product', {
         },
         updateProduct(index) {
             this.selectedVariant = index;
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview);
         }
     }
 });
